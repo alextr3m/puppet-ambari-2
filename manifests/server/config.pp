d51e9a43e8ef88b82b568839fbf11da185bc0563
Nasheb Ismaily <nasheb@gmail.com>
2017-07-26 02:50:45 -0500

    updated code

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -1,27 +1,27 @@
-class ambari::server::config {
+class ambari::server::config() {
 
- # Run ambari's inital setup 
+ $java_home = $::ambari::server::java_home
 
- $java_home =  '/usr/lib/jvm/java-1.8.0-openjdk'
-
- # Iinitial setup ambari server
  file { 
-  'ambari_setup_script' :
-  ensure => 'file',
-  source => 'puppet:///modules/ambari/ambari_setup_script.exp',
-  path => '/tmp/ambari_setup_script.exp',
-  owner => 'root',
-  group => 'root',
-  mode  => '0744',
-  #require => Package['ambari-server'],
-  require => Class['::ambari::server::install'],
-  notify => Exec['run_ambari_server_setup'],
- }
-
+   'ambari_setup_script' :
+   ensure => 'file',
+   source => 'puppet:///modules/ambari/server/ambari_setup_script.exp',
+   path => '/tmp/ambari_setup_script.exp',
+   owner => 'root',
+   group => 'root',
+   mode  => '0777',
+   require => Class['::ambari::server::install'],
+   notify => Exec['run_ambari_server_setup_script']
+ } ->
+# Incase Expect is giving us trouble
+# exec {
+#  'run_ambari_server_setup_script':
+#  command => "/tmp/ambari_setup_script.exp ${java_home}"
+# }
+ # This will run accepting all default values
  exec {
-  'run_ambari_server_setup':
-  command => "/tmp/ambari_setup_script.exp ${java_home}",
-  refreshonly => true
+  'run_ambari_server_setup_script':
+  command => "/usr/sbin/ambari-server setup -j ${java_home} -s"
  }
 
 }
